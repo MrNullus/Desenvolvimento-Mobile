e99e630b5cd9d397cffc4f0cb37974a3baaf1f2d
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-11-12 22:15:38 -0300

    finally pre design

--- Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/TelaPergunta6.java
+++ Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/TelaPergunta6.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TelaPergunta5 extends AppCompatActivity {
+public class TelaPergunta6 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_pergunta5);
+        setContentView(R.layout.activity_tela_pergunta6);
     }
 }
--- Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/TelaPontuacaoFinal.java
+++ Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/TelaPontuacaoFinal.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class TelaPergunta7 extends AppCompatActivity {
+public class TelaPontuacaoFinal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_pergunta7);
+        setContentView(R.layout.activity_tela_pontuacao_final);
     }
 }
