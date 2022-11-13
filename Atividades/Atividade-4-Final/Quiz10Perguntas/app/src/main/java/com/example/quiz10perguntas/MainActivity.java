aa959ce06cc3dfa92d8e060142afa3949ecc5870
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-11-13 00:32:41 -0300

    add new functions

--- Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/MainActivity.java
+++ Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/MainActivity.java
@@ -6,9 +6,36 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtNome;
+    Button btnIniciar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNome    = findViewById(R.id.edtNome);
+        btnIniciar = findViewById(R.id.btnIniciar);
+
+
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                if (edtNome.getText().toString().isEmpty()) {
+                    edtNome.setError("O campo nome é obrigatório!");
+                    edtNome.requestFocus();
+                } else {
+                    Indent telaPergunta1  = new Indent(MainActivity.this, TelaPergunta1.class)
+                    String nomeUsuario = edtNome.getText().toString();
+
+                    telaPergunta1.putExtra("nome_usuario", nomeUsuario);
+
+                    startActivity(telaPergunta1);    
+                }
+            }
+
+        });
     }
 }
--- Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/TelaPergunta1.java
+++ Atividades/Atividade-4-Final/Quiz10Perguntas/app/src/main/java/com/example/quiz10perguntas/TelaPergunta1.java
@@ -6,9 +6,30 @@ import android.os.Bundle;
 
 public class TelaPergunta1 extends AppCompatActivity {
 
+    RadioGroup rgp;
+    Button btnAvancar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_pergunta1);
+
+        rgp        = findViewById(R.id.rgp);
+        btnAvancar = findViewById(R.id.btnAvancar);
+
+
+        btnAvancar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                rdbCheckado = rgp.getCheckedRadioButtonId();
+
+                if (!rdbCheckado.isEmpty()) {
+                    // String nomeUsuario =
+                }
+            }
+
+        });
+
     }
 }
